b76cfc0814d038f71eb845456983c14eb65dab4a
tonioreign <tonioreign@gmail.com>
2022-04-18 09:53:45 -0700

    added js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
-const displayScreen = document.querySelector(".display");
-const inputButtons = document.querySelectorAll("buttons");
+const keys = document.querySelector('.calculator__keys')
+const display = document.querySelector('.calculator__display')
 
+keys.addEventListener('click', (event) => {
+  if (!event.target.closest('button')) return
+  const key = event.target
+  const keyValue = key.textContent
+  const displayValue = display.textContent
 
-const display = () => {
-    if (inputButtons.value)
-}
+  // is this a number key?
+
+  if (key.classList.contains('number')) {
+    if (displayValue === '0') {
+      display.textContent = keyValue
+    } else {
+      display.textContent = displayValue + keyValue
+    }
+  }
+
+  if (key.classList.contains('operator')) {
+    if ()
+  }
+})
